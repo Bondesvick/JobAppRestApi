b9b6229b9006a94734add49cdb986f0aeb26e37f
Victor Nwike <victor.nwike@outlook.com>
2024-05-04 01:11:04 +0100

    initial project setup

---
+++ src/main/java/com/bondesvick/JobAppRestApi/JobAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bondesvick.JobAppRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bondesvick/JobAppRestApi/JobAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondesvick.JobAppRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
